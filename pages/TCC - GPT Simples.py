ea6318d8ebcc10955a47df9636c342f9453cd401
EquipePosCimatec <154333975+EquipePosCimatec@users.noreply.github.com>
2024-06-14 15:16:57 -0300

    TCC - GPT Simples.py

--- pages/TCC - GPT Simples.py
+++ pages/TCC - GPT Simples.py
@@ -10,7 +10,6 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import Chroma
-import Chromadb
 
 # Função para salvar documento em formato .docx no PC do usuário
 def salvar_documento_docx(tipo_documento, conteudo):
