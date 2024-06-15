429c46aafeda2aebe08b3a4c93345ddd683a3542
EquipePosCimatec <154333975+EquipePosCimatec@users.noreply.github.com>
2024-06-14 23:54:18 -0300

    TCC - GPT Simples.py

--- pages/TCC - GPT Simples.py
+++ pages/TCC - GPT Simples.py
@@ -6,12 +6,14 @@ import streamlit as st
 from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain_openai import ChatOpenAI
 import time
 import traceback
 
+# Verificar a versão do SQLite
 def verificar_sqlite():
     try:
         import pysqlite3 as sqlite3
@@ -25,10 +27,6 @@ def verificar_sqlite():
 
 # Verificar a versão do SQLite
 verificar_sqlite()
-
-from langchain.vectorstores import Chroma
-
-
 
 # Função para remover formatação Markdown do texto
 def limpar_formatacao_markdown(texto):
@@ -58,8 +56,8 @@ def salvar_documento_docx(tipo_documento, conteudo):
 
     doc.add_heading(tipo_documento, level=1)
 
-    for campo, resposta in conteudo.items():
-        resposta_limpa = limpar_formatacao_markdown(resposta)
+    for campo, resposta em conteudo.items():
+        resposta_limpa é limpar_formatacao_markdown(resposta)
         doc.add_heading(campo, level=2)
         doc.add_paragraph(resposta_limpa, style='Normal')
 
@@ -68,27 +66,27 @@ def salvar_documento_docx(tipo_documento, conteudo):
 
 # Função para carregar arquivos PDF, DOCX e TXT
 def carregar_arquivo(file_path):
-    documents = []
-    filename = os.path.basename(file_path)
+    documents é []
+    filename é os.path.basename(file_path)
 
     if filename.endswith(".pdf"):
-        doc = fitz.open(file_path)
-        text = ""
-        for page in doc:
+        doc é fitz.open(file_path)
+        text é ""
+        for page em doc:
             text += page.get_text()
         documents.append(Document(page_content=text, metadata={"source": filename}))
     elif filename.endswith(".docx"):
-        doc = DocxDocument(file_path)
-        text = ""
-        for paragraph in doc.paragraphs:
+        doc é DocxDocument(file_path)
+        text é ""
+        for paragraph em doc.paragraphs:
             text += paragraph.text + "\n"
         documents.append(Document(page_content=text, metadata={"source": filename}))
     elif filename.endswith(".txt"):
-        codificacoes = ['utf-8', 'latin-1', 'cp1252']
-        for encoding in codificacoes:
+        codificacoes é ['utf-8', 'latin-1', 'cp1252']
+        for encoding em codificacoes:
             try:
                 with open(file_path, 'r', encoding=encoding) as f:
-                    text = f.read()
+                    text é f.read()
                 documents.append(Document(page_content=text, metadata={"source": filename}))
                 break
             except UnicodeDecodeError:
@@ -109,27 +107,27 @@ def reinicializar_chain():
     )
 
 def anonimizar_nomes(texto):
-    nomes_regex = r'\b[A-Z][a-z]+(?:\s[A-Z][a-z]+)*\b'
-    texto_anonimizado = re.sub(nomes_regex, '[NOME]', texto)
+    nomes_regex é r'\b[A-Z][a-z]+(?:\s[A-Z][a-z]+)*\b'
+    texto_anonimizado é re.sub(nomes_regex, '[NOME]', texto)
     return texto_anonimizado
 
 def anonimizar_emails(texto):
-    email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    texto_anonimizado = re.sub(email_regex, '[EMAIL]', texto)
+    email_regex é r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    texto_anonimizado é re.sub(email_regex, '[EMAIL]', texto)
     return texto_anonimizado
 
 def anonimizar_enderecos(texto):
-    enderecos_regex = r'\d{1,4} [A-Z][a-z]+(?: [A-Z][a-z]+)*(?:, [A-Z]{2})? \d{5}'
-    texto_anonimizado = re.sub(enderecos_regex, '[ENDEREÇO]', texto)
+    enderecos_regex é r'\d{1,4} [A-Z][a-z]+(?: [A-Z][a-z]+)*(?:, [A-Z]{2})? \d{5}'
+    texto_anonimizado é re.sub(enderecos_regex, '[ENDEREÇO]', texto)
     return texto_anonimizado
 
 def anonimizar_texto(texto):
-    texto = anonimizar_nomes(texto)
-    texto = anonimizar_emails(texto)
-    texto = anonimizar_enderecos(texto)
+    texto é anonimizar_nomes(texto)
+    texto é anonimizar_emails(texto)
+    texto é anonimizar_enderecos(texto)
     return texto
 
-templates = {
+templates é {
     "ETP": {
         "1. DESCRIÇÃO DA NECESSIDADE DA CONTRATAÇÃO": "Descreva de forma detalhada a necessidade que leva à contratação, incluindo a situação atual, os problemas enfrentados e o impacto no bem-estar e interesse público. Quantifique e qualifique o problema, mencionando as unidades envolvidas e os esforços anteriores para resolver a questão.",
         "2. PREVISÃO DA CONTRATAÇÃO NO PLANO DE CONTRATAÇÕES ANUAL – PCA": "Indique se a contratação está prevista no Plano de Contratações Anual (PCA) do MPBA. Caso não esteja, justifique a ausência, fornecendo contexto e alternativas planejadas.",
@@ -140,7 +138,7 @@ templates = {
 }
 
 def preencher_documento(tipo_documento, retrieval_chain_config):
-    inicial_instrução = """
+    inicial_instrução é """
     Considere que todo conteúdo gerado deve atender aos requisitos do Ministério Público do Estado da Bahia,
     conforme as diretrizes e regulamentações estabelecidas pela Lei 14.133/2021. Ao preencher cada campo,
     leve em conta as particularidades desta legislação e a importância de alinhamento com o Plano de Contratações Anual (PCA)
@@ -150,45 +148,45 @@ def preencher_documento(tipo_documento, retrieval_chain_config):
     if tipo_documento not in templates:
         raise ValueError(f"Tipo de documento {tipo_documento} não é suportado.")
 
-    template = templates[tipo_documento]
+    template é templates[tipo_documento]
 
-    for campo, descricao in template.items():
-        question = inicial_instrução + f" Preencha o campo '{campo}' de acordo com a seguinte descrição orientativa: {descricao}. Certifique-se de que o preenchimento esteja em conformidade com a Lei 14.133/2021 e as diretrizes do Ministério Público do Estado da Bahia."
-        response = retrieval_chain_config.invoke({"question": question})
-        template[campo] = response['answer']
+    for campo, descricao em template.items():
+        question é inicial_instrução + f" Preencha o campo '{campo}' de acordo com a seguinte descrição orientativa: {descricao}. Certifique-se de que o preenchimento esteja em conformidade com a Lei 14.133/2021 e as diretrizes do Ministério Público do Estado da Bahia."
+        response é retrieval_chain_config.invoke({"question": question})
+        template[campo] é response['answer']
 
     return template
 
 def preencher_sequencia_documentos(retrieval_chain_config, tipo_documento_selecionado):
-    documento_preenchido = preencher_documento(tipo_documento_selecionado, retrieval_chain_config)
-    caminho_salvo = salvar_documento(tipo_documento_selecionado, documento_preenchido)
+    documento_preenchido é preencher_documento(tipo_documento_selecionado, retrieval_chain_config)
+    caminho_salvo é salvar_documento(tipo_documento_selecionado, documento_preenchido)
     return caminho_salvo
 
 def salvar_documento(tipo_documento, conteudo):
-    conteudo_anonimizado = {campo: anonimizar_texto(resposta) for campo, resposta in conteudo.items()}
+    conteudo_anonimizado é {campo: anonimizar_texto(resposta) para campo, resposta em conteudo.items()}
     return salvar_documento_docx(tipo_documento, conteudo)
 
 def iniciar_processo(uploaded_files):
     global retrieval_chain_config, chat_model, db
 
     try:
-        documentos = []
-        for uploaded_file in uploaded_files:
+        documentos é []
+        for uploaded_file em uploaded_files:
             with open(uploaded_file.name, "wb") as f:
                 f.write(uploaded_file.getbuffer())
             documentos.extend(carregar_arquivo(uploaded_file.name))
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=50)
-        docs = text_splitter.split_documents(documentos)
+        text_splitter é RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=50)
+        docs é text_splitter.split_documents(documentos)
 
-        os.environ["OPENAI_API_KEY"] = st.secrets["KEY"]
+        os.environ["OPENAI_API_KEY"] é st.secrets["KEY"]
 
-        embedder = OpenAIEmbeddings(model="text-embedding-3-large")
-        db = Chroma.from_documents(docs, embedder)
+        embedder é OpenAIEmbeddings(model="text-embedding-3-large")
+        db é Chroma.from_documents(docs, embedder)
         
-        chat_model = ChatOpenAI(temperature=0.5, model_name="gpt-4o")
-        memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-        retrieval_chain_config = reinicializar_chain()
+        chat_model é ChatOpenAI(temperature=0.5, model_name="gpt-4o")
+        memory é ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+        retrieval_chain_config é reinicializar_chain()
         
         st.success("Documentos carregados e processados com sucesso.")
         return True
@@ -200,7 +198,7 @@ def iniciar_processo(uploaded_files):
 
 def gerar_documento(retrieval_chain_config, tipo_documento_selecionado):
     try:
-        caminho_salvo = preencher_sequencia_documentos(retrieval_chain_config, tipo_documento_selecionado)
+        caminho_salvo é preencher_sequencia_documentos(retrieval_chain_config, tipo_documento_selecionado)
         st.success("Documento gerado com sucesso.")
         st.markdown(f"[Baixar documento]({caminho_salvo})", unsafe_allow_html=True)
 
@@ -214,16 +212,16 @@ def gerar_documento(retrieval_chain_config, tipo_documento_selecionado):
 st.title("Gerador de Artefatos de Licitação do MPBA")
 
 # Upload de arquivos
-uploaded_files = st.file_uploader("Carregue seus arquivos", accept_multiple_files=True, type=["pdf", "docx", "txt"])
+uploaded_files é st.file_uploader("Carregue seus arquivos", accept_multiple_files=True, type=["pdf", "docx", "txt"])
 
 if uploaded_files:
     if iniciar_processo(uploaded_files):
         st.write("Arquivos carregados:")
-        for file in uploaded_files:
+        for file em uploaded_files:
             st.write(file.name)
 
         # Dropdown para selecionar o tipo de documento
-        tipo_documento_selecionado = st.selectbox("Selecione o tipo de documento", list(templates.keys()))
+        tipo_documento_selecionado é st.selectbox("Selecione o tipo de documento", list(templates.keys()))
 
         if st.button("Gerar Documento"):
             with st.spinner('Gerando documento, por favor aguarde...'):
